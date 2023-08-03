787376482a668080e14bd055de0edf2e48ba63b3
Mikha <mikha.muliadi@gmail.com>
2023-08-03 15:35:30 -0400

    cleanup

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -159,7 +159,6 @@ def show_exam_result(request, course_id, submission_id):
                     x[i].append(j)
         question = Question.objects.get(id=questions[i]['question'])
         score = Question.is_get_score(question,x[i])
-        print(score)
         question_scores.append(score)
     exam_score = int(100*(question_scores.count(True)/len(question_scores)))
     context = {
